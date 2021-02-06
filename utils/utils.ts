a1d0989200bc74c61b20eed9d4772e77faac6a95
Mikael Eriksson <migluuu@gmail.com>
2021-02-07 01:54:32 +0200

    Add utils

--- utils/utils.ts
+++ utils/utils.ts
@@ -12,3 +12,12 @@ export const getColor = (mode: string) => {
       return 'cornflowerblue'
   }
 }
+
+export const secondsToDisplayTime = (totalSeconds: number) => {
+  const hours = Math.floor(totalSeconds / 3600)
+  totalSeconds %= 3600
+  const minutes = Math.floor(totalSeconds / 60)
+  const seconds = totalSeconds % 60
+
+  return (hours > 0 ? `${hours}:` : '') + `${minutes}:${seconds}`
+}

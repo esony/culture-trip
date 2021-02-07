d275ba148a4d8f60d71f4c6fb459823789a0d094
Mikael Eriksson <migluuu@gmail.com>
2021-02-07 11:08:00 +0200

    Extract DestinationCard

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,10 +1,11 @@
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet'
+import { MapContainer, TileLayer, Polyline } from 'react-leaflet'
 import polylineUtil from '@mapbox/polyline'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import css from './Map.module.css'
 import Location from '../../types/Location'
-import { getColor, secondsToDisplayTime } from '../../utils/utils'
+import { getColor } from '../../utils/utils'
+import DestinationCard from './DestinationCard'
 
 type Props = {
   itinerary: any
@@ -13,8 +14,6 @@ type Props = {
 
 const Map = ({ itinerary, destination }: Props) => {
   L.Icon.Default.imagePath = '/'
-
-  const today = new Date().getDay()
 
   return (
     <MapContainer
@@ -29,29 +28,7 @@ const Map = ({ itinerary, destination }: Props) => {
         url="https://cdn.digitransit.fi/map/v1/{id}/{z}/{x}/{y}.png"
       />
       {destination && (
-        <Marker
-          position={[destination.location.lat, destination.location.lon]}
-          title={destination.name.en}
-          alt={destination.name.en}
-        >
-          <Popup className={css.popup}>
-            <img
-              src={destination.description.images[0]?.url}
-              className={css.thumbnail}
-            />
-            <div className={css.description}>
-              <h2>{destination.name.en}</h2>
-              {destination.description.body}
-              <div className={css.openingHours}>
-                Open today: {destination.opening_hours.hours[today].opens} -{' '}
-                {destination.opening_hours.hours[today].closes}
-                <br />
-                Travel time: &nbsp;
-                {itinerary && secondsToDisplayTime(itinerary.duration)}
-              </div>
-            </div>
-          </Popup>
-        </Marker>
+        <DestinationCard destination={destination} itinerary={itinerary} />
       )}
       {itinerary &&
         itinerary.legs.map((leg: any, i: number) => {

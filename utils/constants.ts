6eb27ed4d725fe879587289bf501395ea00c9e04
Mikael Eriksson <migluuu@gmail.com>
2021-02-07 15:03:49 +0200

    Move API url to constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,6 @@
 export const PAGE_TITLE = 'Culture Trip'
 export const PAGE_DESCRIPTION =
   'Take a deep dive into the cultural heart of Helsinki'
+
+export const HSL_GRAPHQL_URL =
+  'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
